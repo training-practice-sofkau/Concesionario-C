6010726a6e047a8cef62e73110ca901552de88cf
Jesus <jesusdavidpoloespitia@gmail.com>
2023-01-23 17:57:10 -0500

    Punto 1
    Se crearon los constructores, los getter y setter, y el metodo To String

--- Concesionario/Coche.cs
+++ Concesionario/Coche.cs
@@ -8,19 +8,33 @@ namespace Concesionario
 {
     class Coche
     {
-        public int ID { get; set; }
-        public string Marca { get; set; }
-        public string Modelo { get; set; }
-        public int KM { get; set; }
-        public int Precio { get; set; }
+        private int _ID;
+        private string _Marca;
+        private string _Modelo;
+        private int _KM;
+        private int _Precio;
 
+
+        //Construcctor
         public Coche(int id, string marca, string modelo, int km, int precio)
         {
-            ID = id;
-            Marca = marca;
-            Modelo = modelo;
-            KM = km;
-            Precio = precio;
+            this._ID = id;
+            this._Marca = marca;
+            this._Modelo = modelo;
+            this._KM = km;
+            this._Precio = precio;
+        }
+
+        //Getters And Setters
+        public int ID { get => _ID; set => _ID = value; }
+        public string Marca { get => _Marca; set => _Marca = value; }
+        public string Modelo { get => _Modelo; set => _Modelo = value; }
+        public int KM { get => _KM; set => _KM = value; }
+        public int Precio { get => _Precio; set => _Precio = value; }
+        //Metodo To String
+        public override string ToString()
+        {
+            return "Marca: " + Marca + " Modelo: " + Modelo + " Kilometros: " + KM + " Precio: " + Precio;
         }
 
     }
--- Concesionario/Program.cs
+++ Concesionario/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Concesionario;
+
+Coche c = new Coche(1, "Ferrari", "2014", 300, 5000);
+
+Console.WriteLine(c.Marca);
+Console.WriteLine(c.Modelo);
+c.KM = 700;
+Console.WriteLine(c.Precio);
+Console.WriteLine(c.KM);
+Console.WriteLine(c.ToString());
+Console.ReadLine();
