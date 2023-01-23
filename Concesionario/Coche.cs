aa96349362275ee8425da2c8560c3ad4fab28926
Adryan Ynfante  Valero <adryan.ynfante@sofka.local>
2023-01-23 09:42:34 -0500

    Talle POO C#
    Concesionario POO

---
+++ Concesionario/Coche.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concesionario
+{
+    class Coche
+    {
+        public int ID { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int KM { get; set; }
+        public int Precio { get; set; }
+
+        public Coche(int id, string marca, string modelo, int km, int precio)
+        {
+            ID = id;
+            Marca = marca;
+            Modelo = modelo;
+            KM = km;
+            Precio = precio;
+        }
+
+    }
+}
