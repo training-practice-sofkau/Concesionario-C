a4fb4eb85d0d3b9b99f390cc139e123785d098f2
David Fernando Briceño Galindo <dbricenogalindo@gmail.com>
2023-01-23 23:40:58 -0500

    Inicializacion proyecto 2.0

--- Concesionario/Coche.cs
+++ Concesionario/Coche.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +10,31 @@ namespace Concesionario
 {
     class Coche
     {
-        public int ID { get; set; }
-        public string Marca { get; set; }
-        public string Modelo { get; set; }
-        public int KM { get; set; }
-        public int Precio { get; set; }
-
+        private int _ID;
+        private string _Marca;
+        private string _Modelo;
+        private int _KM;
+        private int _Precio;
+        public int ID { 
+            get { return _ID; } 
+            set { _ID = value; } 
+        }
+        public string Marca { 
+            get { return _Marca; }
+            set { Marca = value; } 
+        }
+        public string Modelo { 
+            get { return _Modelo; } 
+            set { Modelo = value; } 
+        }
+        public int KM { 
+            get { return _KM; } 
+            set { KM = value; } 
+        }
+        public int Precio { 
+            get { return _Precio;  } 
+            set { _Precio = value; }
+        }
         public Coche(int id, string marca, string modelo, int km, int precio)
         {
             ID = id;
@@ -22,6 +43,9 @@ namespace Concesionario
             KM = km;
             Precio = precio;
         }
-
+        public override string ToString()
+        {
+            return "ID: " + _ID + " Marca: " + _Marca + " Modelo: " + _Modelo +" Kilometraje: " + _KM + " Precio: "+ _Precio;
+        }
     }
 }
