c02eb8fbf8912be7b93b667cb8992209c3b3ca71
Javier Carrillo <javiercamur98@gmail.com>
2023-01-23 22:59:45 -0500

    Se Crea la clase Concesionario y sus metodos de gestion de coches

---
+++ Concesionario/Concesionario.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concesionario
+{
+    internal class Concesionario
+    {
+        private List<Coche> _cocheList;
+        private int _limiteCoches;
+
+        public Concesionario(int limiteCoches)
+        {
+            _cocheList = new List<Coche>();
+            LimiteCoches = limiteCoches;
+        }
+
+        public int LimiteCoches { get => _limiteCoches; set => _limiteCoches = value; }
+
+
+        public void AgregarCoche(Coche coche)
+        {
+            if(_cocheList.Count < _limiteCoches)
+            {
+                _cocheList.Add(coche);
+            }
+        }
+
+        public void EditarCoche(Coche coche)
+        {
+            foreach(Coche aux in _cocheList)
+            {
+                if(aux.ID== coche.ID)
+                {
+                    _cocheList.Remove(aux);
+                }
+            }
+        }
+
+        public void ListarCoches()
+        {
+            foreach(Coche coche in _cocheList)
+            {
+                Console.WriteLine(coche);
+            }
+        }
+
+        public void EliminarCoche(Coche coche)
+        {
+
+        }
+
+    }
+}
