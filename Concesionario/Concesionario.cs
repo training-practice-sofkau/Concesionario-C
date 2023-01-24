7df85d3b7be6cdbeb08e90fdd087032f228d8a5a
Jhoan Gaviria <jmgaviriam@gmail.com>
2023-01-24 09:20:26 -0500

    feat: En la clase Concesionario se crean los métodos añadirCoche(Coche c) editarCoche(Coche c) listarCoches() eliminarCoche(Coche c)

--- Concesionario/Concesionario.cs
+++ Concesionario/Concesionario.cs
@@ -4,8 +4,63 @@ namespace Concesionario
 {
     public class Concesionario
     {
-        public Class1()
+        public Coche[10] coches;
+
+        public Concesionario()
         {
+            coches = new Coche[10];
         }
+
+        public void añadirCoche(Coche coche)
+        {
+            for (int i = 0; i < coches.Length; i++)
+            {
+                if (coches[i] == null)
+                {
+                    coches[i] = coche;
+                    break;
+                }
+            }
+        }
+
+        public void editarCoche(int id, string marca, string modelo, int km, int precio)
+        {
+            for (int i = 0; i < coches.Length; i++)
+            {
+                if (coches[i].ID == id)
+                {
+                    coches[i].Marca = marca;
+                    coches[i].Modelo = modelo;
+                    coches[i].KM = km;
+                    coches[i].Precio = precio;
+                    break;
+                }
+            }
+        }
+
+        public listarCoches()
+        {
+            for (int i = 0; i < coches.Length; i++)
+            {
+                if (coches[i] != null)
+                {
+                    Console.WriteLine(coches[i].toString());
+                }
+            }
+        }
+
+        public void eliminarCoche(int id)
+        {
+            for (int i = 0; i < coches.Length; i++)
+            {
+                if (coches[i].ID == id)
+                {
+                    coches[i] = null;
+                    break;
+                }
+            }
+        }
+
+
     }
 }
