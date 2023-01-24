2791587f12228660c5e201e55d9112767298c823
Jhoan Gaviria <jmgaviriam@gmail.com>
2023-01-24 08:20:44 -0500

    feat: Creación de atributo Transmisión en la clase Cuatrimoto y Sidecar en la clase Moto  con sus respectivas sobreescrituras de los métodos PRecio() y toString().

--- Concesionario/Cuatrimoto.cs
+++ Concesionario/Cuatrimoto.cs
@@ -4,8 +4,36 @@ namespace Concesionario
 {
     public class Cuatrimoto : Coche
     {
-        public Cuatrimoto()
+
+        private bool _transmisionAutomatica;
+
+        public bool TransmisionAutomatica
         {
+            get { return _transmisionAutomatica; }
+            set { _transmisionAutomatica = value; }
+        }
+
+        public Cuatrimoto(int id, string marca, string modelo, int km, int precio, bool transmisionAutomatica)
+        {
+            TransmisionAutomatica = transmisionAutomatica;
+        }
+
+
+        public override Precio()
+        {
+            if (TransmisionAutomatica)
+            {
+                return base.Precio() + 2000000;
+            }
+            else
+            {
+                return base.Precio();
+            }
+        }
+
+        public override string toString()
+        {
+            return base.toString() + " Transmision Automatica: " + TransmisionAutomatica;
         }
     }
 }
--- Concesionario/Moto.cs
+++ Concesionario/Moto.cs
@@ -4,8 +4,36 @@ namespace Concesionario
 {
     public class Moto : Coche
     {
-        public Moto()
+        private bool _sidecar;
+
+        public bool Sidecar
         {
+            get { return _sidecar; }
+            set { _sidecar = value; }
         }
+
+        public Moto(int id, string marca, string modelo, int km, int precio, bool sidecar)
+        {
+            Sidecar = sidecar;
+        }
+
+        public override Precio()
+        {
+            if (Sidecar)
+            {
+                return base.Precio() + 3500000;
+            }
+            else
+            {
+                return base.Precio();
+            }
+        }
+
+        public override string toString()
+        {
+            return base.toString() + " Sidecar: " + Sidecar;
+        }
+
+
     }
 }
