66961191bb880149d463db3b22a24767e88b06b7
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-01-23 20:33:02 -0500

    Modificacion en Class Coche
    Se realiza encapsulacion de propiedades, metodo get y set de cada propiedad y se implementa la sobreescritura de ToString()

--- Concesionario/Program.cs
+++ Concesionario/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Concesionario
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
